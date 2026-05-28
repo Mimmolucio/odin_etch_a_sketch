a98c938047f9a59d81e2081cf85417a46d087007
Mimmolucio <domenico.lp@gmail.com>
2026-05-28 15:25:10 +0200

    dynamic res working variable incl. string interpolation

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const pixel = document.createElement("div")
 
 function createGrid(length) {
     let i = 0
+    const resolution = length*length
+    const width = 320/length
 
-    while (i < length*length) {
+    pixel.style.minWidth = width+"px"
+
+    while (i < resolution) {
         container.appendChild(pixel.cloneNode(true))
         i++
     }
